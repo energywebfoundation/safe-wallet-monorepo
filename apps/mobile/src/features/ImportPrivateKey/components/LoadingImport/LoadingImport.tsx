988475a94cb66bced2d8385292b5bdd55f288203
Nikolaus Wust <nikolaus.wust@energyweb.org>
2025-02-24 14:25:09 +0800

    chore: fix some colours

--- apps/mobile/src/features/ImportPrivateKey/components/LoadingImport/LoadingImport.tsx
+++ apps/mobile/src/features/ImportPrivateKey/components/LoadingImport/LoadingImport.tsx
@@ -5,7 +5,7 @@ import { H4, View } from 'tamagui'
 export function LoadingImportComponent() {
   return (
     <View flex={1} justifyContent="center" alignItems="center">
-      <CircleSnail size={64} color={['#12FF80']} />
+      <CircleSnail size={64} color={['#a466ffbf']} />
       <H4 fontWeight={600} marginTop="$7" marginBottom="$4">
         Creating your signer...
       </H4>
--- apps/mobile/src/theme/palettes/darkPalette.ts
+++ apps/mobile/src/theme/palettes/darkPalette.ts
@@ -5,8 +5,8 @@ const darkPalette = {
     disabled: '#636669',
   },
   primary: {
-    dark: '#0cb259',
-    main: '#12FF80',
+    dark: '#a466ff',
+    main: '#a466ffbf',
     light: '#A1A3A7',
   },
   secondary: {
